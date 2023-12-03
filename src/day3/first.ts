b52856e59027fb40978e979f248c1ca1e5bcc4ac
Hugo Forestier <hugo.forestier123@gmail.com>
2023-12-03 20:35:08 +0100

    had to refacto in order to sleep tonight

--- src/day3/first.ts
+++ src/day3/first.ts
@@ -1,92 +1,57 @@
 import { readFileSync } from 'fs';
 
-const data = readFileSync('./inputs/day3/input-1', 'utf8');
+const data = readFileSync('./inputs/day3/input-1', 'utf8').split('\n').map(x => x.split('')).filter(x => x.length > 0);
 
 var total = 0;
 
-var values: Array<string> = [];
+function isCharNumber(char: string | undefined) {
+    if (char === undefined)
+        return false;
+    return !isNaN(parseInt(char));
+}
 
-data.split('\n').forEach((line: string) => {
-    if (line.length != 0) {
-        values.push(line);
+function isDot(char: string | undefined) {
+    return char == '.';
+}
+
+const dirs = [
+    [-1, 0], [-1, -1], [0, -1],
+    [1, -1], [1, 0], [1, 1],
+    [0, 1], [-1, 1]
+]
+
+function get(y: number, x: number, [dx, dy]: any) {
+    const blob = data[y + dx];
+    if (blob == undefined) {
+        return undefined;
     }
-})
+    return blob[x + dy];
+}
 
 let validated: boolean = false;
-for(let y = 0; y != values.length; y++) {
-    for (let x = 0; x != values[y].length; x++) {
-        if (values[y][x] >= '0' && values[y][x] <= '9') {
-            if (x != 0) {
-                if (values[y][x - 1] != '.' && (values[y][x - 1] < '0' || values[y][x - 1] > '9')) {
-                    validated = true;
-                }
+let isNumber = false;
+let digitToAdd = '';
+for(let y = 0; y < data.length; y++) {
+    for (let x = 0; x < data[y].length; x++) {
+        isNumber = isCharNumber(get(y, x, [0, 0]));
+        if (isNumber) {
+            digitToAdd += get(y, x, [0, 0]);
+        }
+        if (isNumber && !validated) {
+            const toAdd = dirs.reduce((acc, [dy, dx]) => {
+               return acc || !isCharNumber(get(y, x, [dy, dx])) && !isDot(get(y, x, [dy, dx])) && get(y, x, [dy, dx]) !== undefined;
+            }, false);
+            if (toAdd) {
+                validated = true;
             }
-            if (x != 0 && y != 0) {
-                if (values[y - 1][x - 1] != '.' && (values[y - 1][x - 1] < '0' || values[y - 1][x - 1] > '9')) {
-                    validated = true;
-                }
-            }
-            if (y != 0) {
-                if (values[y - 1][x] != '.' && (values[y - 1][x] < '0' || values[y - 1][x] > '9')) {
-                    validated = true;
-                }
-            }
-            if (y + 1 != values.length) {
-                if (values[y + 1][x] != '.' && (values[y + 1][x] < '0' || values[y + 1][x] > '9')) {
-                    validated = true;
-                }
-            }
-            if (y + 1 != values.length && x != 0) {
-                if (values[y + 1][x - 1] != '.' && (values[y + 1][x - 1] < '0' || values[y + 1][x - 1] > '9')) {
-                    validated = true;
-                }
-            }
-            if (x != values[y].length) {
-                if (values[y][x + 1] != '.' && (values[y][x + 1] < '0' || values[y][x + 1] > '9')) {
-                    validated = true;
-                }
-            }
-            if (x != values[y].length && y != 0) {
-                if (values[y - 1][x + 1] != '.' && (values[y - 1][x + 1] < '0' || values[y - 1][x + 1] > '9')) {
-                    validated = true;
-                }
-            }
-            if (x != values[y].length && ((y + 1) != values.length)) {
-                if (values[y + 1][x + 1] != '.' && (values[y + 1][x + 1] < '0' || values[y + 1][x + 1] > '9')) {
-                    validated = true;
-                }
-            }
-        } else {
-            if (x != 0) {
-                if (validated == true) {
-                    let tmp = x;
-                    let tmpNumber: string = '';
-                    if (values[y][x - 1] >= '0' && values[y][x - 1] <= '9') {
-                        tmp--;
-                    }
-                    for (; tmp != -1 && values[y][tmp] >= '0' && values[y][tmp] <= '9'; tmp--);
-                    tmp++;
-                    for (; tmp != values[y].length && values[y][tmp] >= '0' && values[y][tmp] <= '9'; tmp++) {
-                        tmpNumber+=values[y][tmp];
-                    }
-                    total = total + Number(tmpNumber);
-                    validated = false;
-                }
-            }
-            if (validated == true && x == 0 && y != 0) {
-                let tmp = values[y].length - 1;
-                let tmpNumber: string = '';
-                if (values[y - 1][tmp] >= '0' && values[y - 1][tmp] <= '9') {
-                    tmp--;
-                }
-                for (; tmp != -1 && values[y - 1][tmp] >= '0' && values[y - 1][tmp] <= '9'; tmp--);
-                tmp++;
-                for (; tmp != values[y - 1].length && values[y - 1][tmp] >= '0' && values[y - 1][tmp] <= '9'; tmp++) {
-                    tmpNumber+=values[y - 1][tmp];
-                }
-                total = total + Number(tmpNumber);
-                validated = false;
-            }
+        }
+        if (!isNumber && validated == true) {
+            console.log(digitToAdd)
+            total = total + parseInt(digitToAdd);
+            validated = false;
+        }
+        if (!isNumber) {
+            digitToAdd = '';
         }
     }
 }
